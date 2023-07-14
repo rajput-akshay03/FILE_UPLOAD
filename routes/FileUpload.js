8f74cb4e8debb6f9096b66400e79e653dba566f3
rajput-akshay03 <akshayrajput152003@gmail.com>
2023-07-14 17:52:23 +0530

    new

--- routes/FileUpload.js
+++ routes/FileUpload.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const {imgupload,vidupload,imgredupload,localfileupload}= require("../controllers/fileupload")
 
 router.post("/imageupload",imgupload)
-// router.post("/videoupload",vidupload)
-// router.post("/imagereducerupload",imgredupload)
+router.post("/videoupload",vidupload)
+router.post("/imagereducerupload",imgredupload)
 router.post("/localfileupload",localfileupload)
 module.exports = router;
