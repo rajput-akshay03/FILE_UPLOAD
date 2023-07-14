ad180a7ed8f8a3bed32353aa3b1243bae775e90b
rajput-akshay03 <akshayrajput152003@gmail.com>
2023-07-15 00:41:46 +0530

    new changes

--- models/File.js
+++ models/File.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
+const {transporter} = require("../config/nodemailer")
 const fileschema =  new mongoose.Schema(
     {
         name:{
@@ -17,5 +19,26 @@ const fileschema =  new mongoose.Schema(
         }
     }
 )
+// pre and post middlewares are used for taking any action before and after the data entry 
+// always define pre and post middleware before model
+fileschema.post("save",async function(doc){
+         // doc is entry which is saved on the database
+          try{
+         
+                let info =await transporter.sendMail(
+                    {
+                        from:"akshay",
+                        to:doc.email,
+                        subject:"nodemailer se mail ",
+                        html:`<h2>hello bro</h2><p>file uploaded check <a href = "${doc.imageurl}">${doc.imageurl}</a></p>`
+                    }
+                )
+                console.log(info);
+          }
+          catch(err)
+          {
+                console.log(err)
+          }
+}) 
 const File= mongoose.model("File",fileschema)
 module.exports = File;
